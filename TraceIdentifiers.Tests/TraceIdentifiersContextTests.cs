95dc0335de44a15b3065836d73cffbf3b848983f
Ihar Yakimush <IharYakimush@gmail.com>
2019-01-31 16:14:55 +0300

    new identifiers context

---
+++ TraceIdentifiers.Tests/TraceIdentifiersContextTests.cs
@@ -0,0 +1,51 @@
+﻿namespace TraceIdentifiers.Tests
+{
+    using System.Linq;
+
+    using TraceIdentifiers.AspNetCore;
+
+    using Xunit;
+
+    public class TraceIdentifiersContextTests
+    {
+        [Fact]
+        public void CreateAndDisposeSingle()
+        {
+            TraceIdentifiersContext c1 = new TraceIdentifiersContext(true, "qwe");
+            TraceIdentifiersContext c2 = new TraceIdentifiersContext(false, "qwe");
+            using (c1)
+            using (c2)
+            {
+                Assert.Equal("qwe", Assert.Single(c1.Local));
+                Assert.Equal("qwe", Assert.Single(c1.LocalShared));
+
+                Assert.Equal("qwe", Assert.Single(c2.Local));
+                Assert.Empty(c2.LocalShared);
+            }
+        }
+
+        [Fact]
+        public void CreateAndDisposeChilds()
+        {
+            TraceIdentifiersContext c = new TraceIdentifiersContext(true, "qwe");
+
+            using (var c1 =c.CreateChild(false,"c1"))
+            {
+                using (var c11 = c.CreateChild(false, "c11"))
+                {
+
+                }
+
+                using (var c12 = c.CreateChild(false, "c11"))
+                {
+
+                }
+            }
+
+            using (var c2 = c.CreateChild(false, "c2"))
+            {
+                
+            }
+        }
+    }
+}
---
+++ TraceIdentifiers/TraceIdentifiersContext.cs
@@ -0,0 +1,105 @@
+﻿namespace TraceIdentifiers.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TraceIdentifiersContext : IDisposable
+    {
+        private static readonly char[] Chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        private Stack<KeyValuePair<string,bool>> local;
+
+        private string localBookmark;
+
+        private const int MaxNestedLevel = 1000;
+
+        public static string GetNonSecureRandomString(int length = 8)
+        {
+            if (length < 1 || length > 16)
+            {
+                throw new ArgumentException();
+            }
+
+            StringBuilder result = new StringBuilder(length);
+            foreach (byte b in Guid.NewGuid().ToByteArray().Take(length))
+            {
+                result.Append(Chars[b % Chars.Length]);
+            }
+
+            return result.ToString();
+        }
+
+        public IEnumerable<string> Local => this.local.Select(p => p.Key);
+
+        public IEnumerable<string> LocalShared => this.local.Where(p => p.Value).Select(p => p.Key);
+
+        public string Remote { get; set; }
+
+        public LinkedList<Stack<string>> RemoteShared { get; set; } = new LinkedList<Stack<string>>();
+
+        public TraceIdentifiersContext(
+            bool shared = true,
+            string traceIdentifier = null)
+        {
+            this.local = new Stack<KeyValuePair<string, bool>>();
+
+            this.AddIdentifierToLocal(shared, traceIdentifier);
+        }
+
+        private void AddIdentifierToLocal(bool shared, string traceIdentifier)
+        {
+            if (traceIdentifier != null)
+            {
+                if (this.local.Any(p => p.Key == traceIdentifier))
+                {
+                    throw new ArgumentException($"Identifier with same name {traceIdentifier} already used.");
+                }
+            }
+
+            if (this.local.Count >= MaxNestedLevel)
+            {
+                throw new InvalidOperationException($"Limit of {MaxNestedLevel} for nested child elements exceeded");
+            }
+
+            if (traceIdentifier == null)
+            {
+                do
+                {
+                    traceIdentifier = GetNonSecureRandomString();
+                }
+                while (this.local.Any(p => p.Key == traceIdentifier));
+            }
+
+            this.local.Push(new KeyValuePair<string, bool>(traceIdentifier, shared));
+            this.localBookmark = traceIdentifier;
+        }
+
+        private TraceIdentifiersContext(Stack<KeyValuePair<string, bool>> local, string traceIdentifier, bool shared)
+        {
+            this.local = local;
+
+            this.AddIdentifierToLocal(shared, traceIdentifier);
+        }
+
+        public TraceIdentifiersContext CreateChild(bool shared = true, string local = null)
+        {
+            if (this.localBookmark == this.local.Peek().Key)
+            {
+                return new TraceIdentifiersContext(this.local, local, shared);
+            }
+            
+            throw new InvalidOperationException("Unable to create child context, because previous context with same nested level not disposed");
+        }
+
+        public void Dispose()
+        {
+            if (this.localBookmark == this.local.Peek().Key)
+            {
+                this.local.Pop();
+            }            
+        }
+    }
+}
