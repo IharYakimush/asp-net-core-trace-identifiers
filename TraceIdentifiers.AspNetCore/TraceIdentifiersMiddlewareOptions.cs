a8c5a4c30e0d620ed0eda18768d68e35c343f33a
Ihar Yakimush <IharYakimush@gmail.com>
2019-05-16 16:15:28 +0300

    refactor

--- TraceIdentifiers.AspNetCore/TraceIdentifiersMiddlewareOptions.cs
+++ TraceIdentifiers.AspNetCore/TraceIdentifiersMiddlewareOptions.cs
@@ -13,16 +13,16 @@ namespace TraceIdentifiers.AspNetCore
         public Func<HttpContext, bool> LocalIsShared { get; set; } = (c) => true;
         public Func<HttpContext, string> LocalValueFactory { get; set; } = (c) => c.TraceIdentifier;
 
-        public Func<HttpContext, string> WriteLocalHeaderName { get; set; } = (c) => TraceIdentifiersSendOptions.DefaultHeaderName;
+        public Func<HttpContext, string> WriteLocalHeaderName { get; set; } = (c) => TraceIdentifiersDefaults.DefaultHeaderName;
 
         public Func<HttpContext, bool> ReadRemote { get; set; } = (c) => true;
 
-        public Func<HttpContext, string> ReadRemoteHeaderName { get; set; } = (c) => TraceIdentifiersSendOptions.DefaultHeaderName;
+        public Func<HttpContext, string> ReadRemoteHeaderName { get; set; } = (c) => TraceIdentifiersDefaults.DefaultSharedHeaderName;
 
-        public Func<HttpContext, char> ReadRemoteSeparator { get; set; } = (c) => TraceIdentifiersSendOptions.DefaultSeparator;
+        public Func<HttpContext, char> ReadRemoteSeparator { get; set; } = (c) => TraceIdentifiersDefaults.DefaultSeparator;
 
-        public Func<HttpContext, int> ReadRemoteMaxCount { get; set; } = (c) => TraceIdentifiersSendOptions.DefaultIdentifiersMaxCount;
+        public Func<HttpContext, int> ReadRemoteMaxCount { get; set; } = (c) => TraceIdentifiersDefaults.DefaultIdentifiersMaxCount;
 
-        public Func<HttpContext, int> ReadRemoteMaxLength { get; set; } = (c) => TraceIdentifiersSendOptions.DefaultIdentifierMaxLength;
+        public Func<HttpContext, int> ReadRemoteMaxLength { get; set; } = (c) => TraceIdentifiersDefaults.DefaultIdentifierMaxLength;
     }
 }
---
+++ TraceIdentifiers/TraceIdentifiersDefaults.cs
@@ -0,0 +1,15 @@
+﻿namespace TraceIdentifiers
+{
+    public class TraceIdentifiersDefaults
+    {
+        public const string DefaultHeaderName = "X-TraceIdentifier";
+
+        public const string DefaultSharedHeaderName = "X-TraceIdentifiers";
+
+        public const char DefaultSeparator = '|';
+
+        public const int DefaultIdentifiersMaxCount = 10;
+
+        public const int DefaultIdentifierMaxLength = 32;
+    }
+}
